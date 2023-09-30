355b4e1444d69472a91bc9f6dfb1aec26f8bdc07
Sushil97 <sushil500530@gmail.com>
2023-09-30 08:32:08 +0600

    set user profile

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -2,17 +2,21 @@ import { useContext } from "react";
 import { AuthContex } from "../../providers/authProvder/AuthProvider";
 
 const Profile = () => {
-    const {user} = useContext(AuthContex) ;
+    const { user } = useContext(AuthContex);
     console.log(user);
     return (
         <div>
+
             <h3 className="text-3xl text-center font-bold my-5">Your Personal Profile</h3>
-            <div className="mt-8">
+            <div className="mt-8 text-xl font-bold">
+                <div className="flex gap-8"><div className="w-36 h-36"><img src={user.photoURL} alt="image" className="w-full h-full rounded-full" /></div> </div>
                 <h4>Name:{user.displayName}</h4>
                 <h4>Email: {user.email}</h4>
-                <h4>Email Verified: {user.emailVerified}</h4>
-                <div className="flex gap-8">photo: <div className="w-36 h-36 rounded-full"><img src={user.photoURL} alt="image" className="w-full h-full rounded-full" /></div> </div>
-                <h4>Phone Number: {user.phoneNumber}</h4>
+                <h4 className="flex gap-2">Email Verified: {user.emailVerified ? <p>True</p> : <p>False</p>}</h4>
+
+                <h4 className="flex gap-3">Phone Number:  {user.phoneNumber ? user.phoneNumber : <p>not found</p>}</h4>
+                <h3>Sign in Creation Time: {user.metadata.creationTime}</h3>
+                <h3>Last Sign in Time: {user.metadata.lastSignInTime}</h3>
             </div>
         </div>
     );
